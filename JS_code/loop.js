ad1b59cb4ef1739d14a9aeb43291365c86a6939a
Md Jewel <md.jewel.cse@gmail.com>
2019-11-22 21:57:55 +0600

    find id using loop

--- JS_code/loop.js
+++ JS_code/loop.js
@@ -1,9 +1,18 @@
-const cats = ['Bill', 'Jeff', 'Pete', 'Biggles', 'Jasmin'];
-let info = 'My cats are called ';
+const contacts = ['jewel:11221212','shohel:112243','Chris:2232322', 'Sarah:3453456', 'Bill:7654322'];
 const para = document.querySelector('p');
-
-for (let i = 0; i < cats.length; i++) {
-  info += cats[i] + ', ';
-}
-
-para.textContent = info;
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+btn.addEventListener('click', function() {
+    let searchName = input.value.toLowerCase();
+    input.value = '';
+    input.focus();
+    for (let i = 0; i < contacts.length; i++) {
+      let splitContact = contacts[i].split(':');
+      if (splitContact[0].toLowerCase() === searchName) {
+        para.textContent = splitContact[0] + '\'s number is ' + splitContact[1] + '.';
+        break;
+      } else {
+        para.textContent = 'Contact not found.';
+      }
+    }
+  });
